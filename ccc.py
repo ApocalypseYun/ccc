1f0f0c552b7a64632adbf12afc5cfbf2d90331e4
TaQini <742954809@qq.com>
2016-10-01 11:29:09 +0800

    ver0.2
    add print lines feature

--- ccc.py
+++ ccc.py
@@ -4,21 +4,32 @@
 from sys import *
 
 notcc = ' \n!"\'#$%&()*+,-./:;<=>?@[\\]^_`{|}~'
-num = 0
-
+chars = 0
+words = 0
+lines = 0
 def usage():
     print "usage: " + argv[0] + " <filename>"
     exit(-1)
 
-if len(argv) < 2:
-    usage()
+if __name__ == '__main__':
+    if len(argv) < 2:
+        usage()
+    
+    try:
+        for line in open(argv[1]):
+            chars += len(line.decode("utf-8"))
+            lines += 1
+            
+            for c in line:
+                if c in notcc:
+                    chars -= 1
 
-try:
-    for line in open(argv[1]):
-        num += len(line.decode("utf-8"))
-        for c in line:
-            if c in notcc:
-                num -= 1
-    print num
-except IOError:
-    usage()
+        print "chars: " + str(chars) + ', lines: ' + str(lines)
+    
+    except IOError:
+        usage()
+        
+# 待开发
+# 1.通过管道通信 实现字符统计 
+#   e.g. cat flag.txt | cwc.py 
+#   out: chars: xx, lines; yy
